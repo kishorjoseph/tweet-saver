216ae386558f59659867b343eb649357f6171166
kishorjoseph <profile.kishor@gmail.com>
2021-01-31 13:19:34 +0000

    tweet-saver application

---
+++ twitter-assignment/tweet-saver/src/components/TweetBucket.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { TweetItem } from "./TweetItem";
+export const TweetBucket = (props) => {
+  return (
+    <div
+      className="tweet-lists"
+      onDragLeave={(e) => props.onDragLeave(e)}
+      onDragEnter={(e) => props.onDragEnter(e)}
+      onDragEnd={(e) => props.onDragEnd(e)}
+      onDragOver={(e) => props.onDragOver(e)}
+      onDrop={(e) => props.onDrop(e)}>
+      {props.tweets.map((tweet, index) => (
+        <TweetItem
+          key={index}
+          tweet={tweet}
+          onDragStart={props.onDragStart}
+          onDragEnd={props.onDragEnd}
+        />
+      ))}
+    </div>
+  );
+};
